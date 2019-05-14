23cb15ccdab4d082c6ed32e0fbe9f2487f46e8de
Julie <sizumasa@hotmail.com>
2019-05-15 00:03:33 +0800

    jest 試半天還是沒裝好

---
+++ src/app.test.js
@@ -0,0 +1,8 @@
+import { shallow, mount, render } from 'enzyme';
+import Intro from './containers/intro';
+import Rule from './components/rule';
+
+test('子 Component 存在', () => {
+    const wrapper = shallow(<Intro />);
+    expect(wrapper.find(Rule).length).toBe(1);
+  });
